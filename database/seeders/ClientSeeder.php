0238331a923005f9daf6aceaa7a7dcde17957a7b
Keith Jordan <shadow902@gmail.com>
2024-10-31 13:22:02 -0400

    update client/committee seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -50,7 +50,7 @@ class ClientSeeder extends Seeder
         Client::factory()->create([
             'contact_name'         => 'Keith Jordan',
             'contact_email'        => 'shadow902@gmail.com',
-            'contact_phone_number' => '540-521-8487'
+            'contact_phone_number' => '540-521-8487',
             'company_name'         => 'Policies and Proceedures',
             'company_address'      => '216 12th Street, Roanoke, VA, 24017',
             'company_city'         => 'Roanoke',
@@ -62,7 +62,7 @@ class ClientSeeder extends Seeder
             'contact_name'         => 'Jeff Williams',
             'contact_email'        => fake()->unique()->safeEmail(),
             'contact_phone_number' => fake()->phoneNumber(),
-            'company_name'         => 'Talent Hunt'
+            'company_name'         => 'Talent Hunt',
             'company_address'      => '216 12th Street, Roanoke, VA, 24017',
             'company_city'         => 'Roanoke',
             'company_zip'          => '24017',
