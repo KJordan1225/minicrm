d7723a7ec2928ea6eb47c879403db81b42e193b1
Keith Jordan <shadow902@gmail.com>
2024-11-05 19:11:00 -0500

    update tasks edit view

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -14,31 +14,31 @@
                         @csrf
                         @method('PUT')
 
-                        <!-- Title -->
-                        <div>
-                            <x-input-label for="title" :value="__('Title')" />
-                            <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title', $task->title)" required />
-                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
-                        </div>
-
-                        <!-- Description -->
-                        <div class="mt-4">
-                            <x-input-label for="description" :value="__('Description')" />
-                            <textarea id="description" class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="description" required>{{ old('description', $task->description) }}</textarea>
-                            <x-input-error :messages="$errors->get('description')" class="mt-2" />
-                        </div>
-
-                        <!-- Deadline At -->
-                        <div class="mt-4">
-                            <x-input-label for="deadline_at" :value="__('Deadline')" />
-                            <x-text-input id="deadline_at" class="block mt-1 w-full" type="date" name="deadline_at" min="{{ today()->format('Y-m-d') }}" :value="old('deadline_at', $task->deadline_at)" required />
-                            <x-input-error :messages="$errors->get('deadline_at')" class="mt-2" />
-                        </div>
+                        <!-- Title Input -->
+						<div class="mb-3">
+							<label for="title" class="form-label">Title</label>
+							<input type="text" class="form-control" id="title" name="title">
+							<x-input-error :messages="$errors->get('title')" class="mt-2" />
+						</div>                        
+						
+						<!-- Description -->
+						<div class="mb-3">
+							<label for="description" class="form-label">Description</label>
+							<textarea class="form-control" id="description" name="description"></textarea>
+							<x-input-error :messages="$errors->get('description')" class="mt-2" />
+						</div>                        
+						
+						<!-- Deadline At -->
+						<div class="mt-4">
+							<label for="deadline_at" class="form-label">Deadline</label>
+							<input type="date" class="form-control" id="deadline_at" name="deadline_at" min="{{ today()->format('Y-m-d') }}" value="{{ old('deadline_at', $task->deadline_at) }}">
+							<x-input-error :messages="$errors->get('deadline_at')" class="mt-2" />
+						</div>
 
                         <!-- Assigned User -->
                         <div class="mt-4">
-                            <x-input-label for="user_id" :value="__('Assigned user')" />
-                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="user_id" id="user_id">
+							<label for="user_id" class="form-label">Assigned user</label>
+                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm form-select" name="user_id" id="user_id">
                                 @foreach($users as $user)
                                     <option value="{{ $user->id }}"
                                         @selected(old('user_id', $task->user_id) == $user->id)>{{ $user->first_name . ' ' . $user->last_name }}</option>
@@ -49,8 +49,8 @@
 
                         <!-- Assigned Committee -->
                         <div class="mt-4">
-                            <x-input-label for="client_id" :value="__('Assigned committee')" />
-                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="client_id" id="client_id">
+							<label for="client_id" class="form-label">Assigned committee</label>
+                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm form-select" name="client_id" id="client_id">
                                 @foreach($clients as $client)
                                     <option value="{{ $client->id }}"
                                         @selected(old('client_id', $task->client_id) == $client->id)>{{ $client->company_name }}</option>
@@ -61,8 +61,8 @@
 
                         <!-- Assigned Project -->
                         <div class="mt-4">
-                            <x-input-label for="project_id" :value="__('Assigned project')" />
-                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="project_id" id="project_id">
+							<label for="project_id" class="form-label">Assigned project</label>
+                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm form-select" name="project_id" id="project_id">
                                 @foreach($projects as $project)
                                     <option value="{{ $project->id }}"
                                         @selected(old('project_id', $task->project_id) == $project->id)>{{ $project->title }}</option>
@@ -73,21 +73,21 @@
 
                         <!-- Status -->
                         <div class="mt-4">
-                            <x-input-label for="status" :value="__('Status')" />
-                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="status" id="status">
+							<label for="status" class="form-label">Status</label>
+                            <select class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm form-select" name="status" id="status">
                                 @foreach(\App\Enums\TaskStatus::cases() as $status)
                                     <option value="{{ $status->value }}"
-                                        @selected(old('status', $task->status->value) == $status->value)>{{ $status->value }}</option>
-                                @endforeach
-                            </select>
-                            <x-input-error :messages="$errors->get('status')" class="mt-2" />
-                        </div>
+												@selected(old('status', $task->status->value) == $status->value)>{{ $status->value }}</option>
+										@endforeach
+									</select>
+									<x-input-error :messages="$errors->get('status')" class="mt-2" />
+								</div>
 
-                        <x-primary-button class="mt-4">
-                            {{ __('Save') }}
-                        </x-primary-button>
+								<!-- Submit Button -->
+								<div class="d-flex justify-content-center mt-4">
+									<button type="submit" class="btn btn-primary w-50 mt-3" style="background-color: #6D2077; color: #B7A57A;">Save</button>
+								</div>   
                     </form>
-
                 </div>
             </div>
         </div>
