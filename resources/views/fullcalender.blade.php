648ee52f0b04ac6bfd40fb87769c8c73d12959ca
Keith Jordan <shadow902@gmail.com>
2024-12-01 14:50:01 -0500

    add TinyMCE to FullCalendar textareas

--- resources/views/fullcalender.blade.php
+++ resources/views/fullcalender.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>GA WEBAPP FULLCALENDAR</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite(['resources/js/app.js'])
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        @vite(['resources/js/app.js'])
     
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
